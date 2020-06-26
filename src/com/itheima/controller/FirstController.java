e82fdaab8c36f40df9143ea61bcb24db2a5a9dc4
yuntsy <2399732689@qq.com>
2020-06-26 15:05:41 +0800

    eclipse springmvc

---
+++ src/com/itheima/controller/FirstController.java
@@ -0,0 +1,18 @@
+package com.itheima.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class FirstController implements Controller{
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request,HttpServletResponse response){
+		ModelAndView mav=new ModelAndView();
+		mav.addObject("msg","这是我的第一个springmvc程序");
+		mav.setViewName("/WEB-INF/jsp/first.jsp");
+		return mav;
+	}
+
+}
